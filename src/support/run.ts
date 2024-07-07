3a06a7b9b2d75f31d3f0d0d8f1214ee0078e3c80
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-07 18:59:33 -0500

    refactor: Remove unused code and update browser handling in run.ts

--- src/support/run.ts
+++ src/support/run.ts
@@ -1,27 +1,16 @@
 import { Browser, chromium } from 'playwright';
-import { BeforeAll, AfterAll, Before, After, setDefaultTimeout } from '@cucumber/cucumber';
-import { CustomWorld } from './custom-world';
+import { BeforeAll, AfterAll, setDefaultTimeout } from '@cucumber/cucumber';
 
 setDefaultTimeout(60000);
 
-declare global {
-    var browser: any;
-}
+let browser: Browser;
 
+// Launch the browser before all tests
 BeforeAll(async function () {
-    global.browser = await chromium.launch({ headless: false });
+    browser = await chromium.launch({ headless: false });
 });
 
+// Close the browser after all tests
 AfterAll(async function () {
-    await global.browser.close();
-});
-
-Before(async function (this: CustomWorld) {
-    this.context = await global.browser.newContext();
-    this.page = await this?.context?.newPage();
-});
-
-After(async function (this: CustomWorld) {
-    await this.page?.close();
-    await this.context?.close();
+    await browser.close();
 });
