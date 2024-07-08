739a2d06df669b088ad478e050996f978c34eb2b
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-07 20:57:01 -0500

    chore: Update cucumber.js configuration for Cucumber test execution

--- cucumber.js
+++ cucumber.js
@@ -1,13 +1,15 @@
+const path = require('path');
 module.exports = {
   default: {
     requireModule: ['ts-node/register'],
-    require: ['src/support/run.ts', 'src/support/hooks.ts','src/step-definitions/**/*.ts', 'src/support/**/*.ts'],
+    require: ['src/support/run.ts', 'src/support/hooks.ts', 'src/step-definitions/**/*.ts', 'src/support/**/*.ts'],
     format: [
-      '@cucumber/pretty-formatter',
-      'progress-bar',
-      'html:cucumber-report.html',
-      'json:cucumber-report.json',
-      'allure-cucumberjs/dist/allure-cucumber'
+      "@cucumber/pretty-formatter",
+      "progress-bar",
+      "html:cucumber-report.html",
+      "json:cucumber-report.json",
+      // `${path.resolve(__dirname, 'config/reporter.js')}`,
+      "./config/reporter.js"
     ],
     retry: 2,
     parallel: 2,
